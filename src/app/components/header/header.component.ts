496a7209710ac3002a7cdb8a426b01bd97dd3556
jcasben <jcasben02@gmail.com>
2025-05-29 19:48:01 +0200

    feat: made header responsive, with burger menu for the navbar when in smaller screens

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,23 @@
 import {Component, inject} from '@angular/core';
 import {TranslatePipe, TranslateService} from '@ngx-translate/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   imports: [
     TranslatePipe,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    NgClass
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   protected translate = inject(TranslateService);
+
+  protected isMenuOpen = false;
 
   protected changeLang(event: Event) {
     const selectElement = event.target as HTMLSelectElement;
