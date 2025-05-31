c73d6fea0f9cf2f396eb30125f603d69677041d3
jcasben <jcasben02@gmail.com>
2025-06-01 00:53:55 +0200

    imported routing and translating service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {TranslateService} from '@ngx-translate/core';
+import {Component} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {TranslatePipe, TranslateService} from '@ngx-translate/core';
 import {HeaderComponent} from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, RouterLink, TranslatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
