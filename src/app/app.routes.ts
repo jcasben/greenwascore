9d1b219d04da399f317c6d01c72793a6cde15af1
jcasben <jcasben02@gmail.com>
2025-06-01 01:05:01 +0200

    new: added route for legal and copyright

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
-import { Routes } from '@angular/router';
-import {HomeComponent} from './pages/home/home.component';
-import {RubricComponent} from './pages/rubric/rubric.component';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
+import { RubricComponent } from "./pages/rubric/rubric.component";
+import { LegalComponent } from "./pages/legal/legal.component";
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'rubric', component: RubricComponent}
+  { path: "", component: HomeComponent },
+  { path: "rubric", component: RubricComponent },
+  { path: "legal", component: LegalComponent },
 ];
