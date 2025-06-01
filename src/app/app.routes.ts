1d4b8d0024bc37f62364e101cc0ab4ac7921bd5a
jcasben <jcasben02@gmail.com>
2025-06-01 21:29:38 +0200

    created new route for evaluation detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from "./pages/home/home.component";
 import { RubricComponent } from "./pages/rubric/rubric.component";
 import { LegalComponent } from "./pages/legal/legal.component";
 import { EvaluationsComponent } from "./pages/evaluations/evaluations.component";
+import { EvaluationDetailComponent } from "./pages/evaluation-detail/evaluation-detail.component";
 
 export const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "rubric", component: RubricComponent },
   { path: "evaluations", component: EvaluationsComponent },
+  { path: "evaluations/:id", component: EvaluationDetailComponent },
   { path: "legal", component: LegalComponent },
 ];
