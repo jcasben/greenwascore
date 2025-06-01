a5789c524f4d50fcc6b25880e9b2f1a487fee2fd
jcasben <jcasben02@gmail.com>
2025-06-01 18:06:38 +0200

    new: created evaluations route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from "@angular/router";
 import { HomeComponent } from "./pages/home/home.component";
 import { RubricComponent } from "./pages/rubric/rubric.component";
 import { LegalComponent } from "./pages/legal/legal.component";
+import { EvaluationsComponent } from "./pages/evaluations/evaluations.component";
 
 export const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "rubric", component: RubricComponent },
+  { path: "evaluations", component: EvaluationsComponent },
   { path: "legal", component: LegalComponent },
 ];
