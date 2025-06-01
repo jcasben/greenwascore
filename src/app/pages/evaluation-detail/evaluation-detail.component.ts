a5eb6f777a85590c78dc58378c2b7c0c2f9eecb7
jcasben <jcasben02@gmail.com>
2025-06-01 22:12:32 +0200

    created a component to view the detail of the results of an evaluation

---
+++ src/app/pages/evaluation-detail/evaluation-detail.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject } from "@angular/core";
+import { Evaluation } from "../../models/evaluation";
+import { Router } from "@angular/router";
+import { TranslatePipe } from "@ngx-translate/core";
+import { NgClass } from "@angular/common";
+
+@Component({
+  selector: "app-evaluation-detail",
+  imports: [TranslatePipe, NgClass],
+  templateUrl: "./evaluation-detail.component.html",
+  styleUrl: "./evaluation-detail.component.css",
+})
+export class EvaluationDetailComponent {
+  private router = inject(Router);
+
+  protected evaluation: Evaluation;
+
+  constructor() {
+    const navigation = this.router.getCurrentNavigation();
+    this.evaluation = navigation?.extras?.state?.["evaluation"];
+  }
+
+  colorClass(): string {
+    if (this.evaluation.result <= 65) return "text-red-500";
+    if (this.evaluation.result >= 66 && this.evaluation.result <= 105)
+      return "text-yellow-500";
+    return "text-green-500";
+  }
+}
