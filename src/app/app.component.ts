aeff0a122942dcb9888e5fe91643fbcc6e9bd0aa
jcasben <jcasben02@gmail.com>
2025-08-25 16:16:12 +0200

    [UPDATE]: changed some styles to make the page more modern

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import {Component} from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
 import {TranslatePipe, TranslateService} from '@ngx-translate/core';
 import {HeaderComponent} from './components/header/header.component';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, RouterLink, TranslatePipe],
+  imports: [RouterOutlet, HeaderComponent, RouterLink, TranslatePipe, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  constructor(translate: TranslateService) {
+  constructor(translate: TranslateService, public router: Router) {
     translate.addLangs(['en', 'es', 'cat']);
     const browserLang = translate.getBrowserLang();
     const localStorageLang = localStorage.getItem('lang');
