7645325120a834fbef51a9c196ab0e1b898f7736
Jesus Castillo <jesuscastillobenito4@gmail.com>
2025-09-08 16:48:37 +0200

    [UPDATE]: created download buttons for guide and indicators

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {TranslatePipe} from '@ngx-translate/core';
+import {Component, inject} from '@angular/core';
+import {TranslatePipe, TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +9,18 @@ import {TranslatePipe} from '@ngx-translate/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {}
+export class HomeComponent {
+  private translate = inject(TranslateService)
+
+  get guidePath(): string {
+    if (this.translate.currentLang === 'en') return 'docs/support-guide-rubric-use.pdf'
+    else if (this.translate.currentLang === 'es') return 'docs/guia-apoyo-uso-rubrica.pdf'
+    else return 'docs/guia-support-us-rubrica.pdf'
+  }
+
+  get indicatorsPath(): string {
+    if (this.translate.currentLang === 'en') return 'docs/greenwashing-indicators.pdf';
+    else if (this.translate.currentLang === 'es') return 'docs/indicadores-greenwashing.pdf';
+    else return 'docs/indicadors-greenwashing.pdf';
+  }
+}
